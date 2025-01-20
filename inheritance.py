00f618062848a3e93648966e17d991791f9fbadd
Shreya <shreyastha1278@gmail.com>
2025-01-20 08:54:14 +0545

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,24 @@
+# Types of inheritance
+# single inheritance
+# multiple inheritance(java doesnot support this inheritance)
+# multilevel inheritance
+# hybrid inheritance
+
+# class Animal:
+#     def animal(self):
+#         print("Animal")
+
+# class Dog:
+#     def dog(self):
+#         print("Dog lives in home")
+
+# class pug:
+#     def pug(self):
+#         print("Pug is the breed of dog")
+
+# ani=Animal()
+# ani.animal()
+# dog=Dog()
+# dog.dog()
+# pug=pug()
+# pug.pug()
