cb75e0a84b845dc799714a105a7a253bddd8fa7d
Shreya <shreyastha1278@gmail.com>
2025-01-20 08:16:52 +0545

    datatypes_function

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+#python datatypes
+# #primitive datatype(value bata detect hunxa
+# #a=10
+# #print (id(a))
+# #a=20
+# #print(id(a))
+
+# #datatypes=int,float,string,boolean
+
+# true,false(0,1)
+
+# a=10
+# print(type(a))
+
+# #float
+# b=20.22
+# print(type(b))
+
+# #string
+# name='shreya'
+# print(type(name))
+
+# poem='''
+# twinkle twinkle little star
+# how i wonder what you are
+# up above the world so high 
+# like a diamond in the sky
+# '''
+
+# print(poem)
+# print(type(poem))
+
+# reference datatype
+# list,tuple,set,dictionary
