85c8323bc769d1067836aed77bd7ae55902fb4f2
Shreya <shreyastha1278@gmail.com>
2025-01-20 08:19:10 +0545

    exceptional_handling_function

---
+++ exceptional_handling.py
@@ -0,0 +1,23 @@
+# def division(x,y):
+#     return x/y
+
+# firstnumber=int(input("Enter first number:"))
+# secondnumber=int(input("Enter second number:"))
+# d=division(firstnumber,secondnumber)
+# print(d)
+
+# TRY,EXCEPT,ELSE,FINALLY 
+
+# numbers=[1,2,3,4,5,6]
+# try:
+#     print(numbers[6])
+# except Exception as e:
+#     print(e)
+
+# x=int(input("Enter a number:"))
+
+# try:
+#     if x<0:
+#         raise Exception("the number must not be less than zero")
+# except Exception as e:
+    # print(e)
