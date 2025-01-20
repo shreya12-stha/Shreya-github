5c3236a995197370a18982ef884215d8cd5def81
Shreya <shreyastha1278@gmail.com>
2025-01-20 08:07:22 +0545

    conditional_statement

---
+++ conditional_statement.py
@@ -0,0 +1,52 @@
+##if,elif,else
+#if=true matrai run hunxa
+#elif=false matrai run hunxa
+#else=true run vayena vani arko condition hunxa
+
+# day=input("enter a day:")
+# if day =="saturday":
+    # print("xya aja tw kastyo padhna paryo")
+
+# number/2 remainder 0=even number
+# number/2 remainder not 0=odd number 
+
+# number=int(input("enter a number:"))
+# if number%2!= 0:
+#      print("odd number")
+# else:
+#     print("even number"
+
+
+# number=int(input("enter a number: "))
+# if number>0:
+    #  print("It is positive number: ")
+# elif number<0:
+    # print("It is negative number: ")
+# else:
+    #  print("It is zero: ")
+
+
+# number=int(input("enter a number: "))
+# if (number%5)==0 and (number%3)==0:
+#     print("It is divisible by 5 and 3")
+# else:
+#     print("It is not divisible by 5 and 3")
+
+
+# number
+# number greater than 10
+# number greater than 20
+# greater then 10 and 20
+# '''
+# #num
+# num=int(input("enter a number"))
+# if num>10: 
+#     if num>20:
+#        print("the number is greater than 20")
+#     else: 
+#      print("the number is greater than 20 only")
+# else:
+#    print("the number is greater than 10 only")'''
+
+
+# provide me min.10 question of python of if else condition
