3b20a4bc1311d77028bea339f4f530f04a95b159
Shreya <shreyastha1278@gmail.com>
2025-01-20 08:32:17 +0545

    generator

---
+++ generator.py
@@ -0,0 +1,11 @@
+# def file_read(path):
+    # with open(path,'r')as file:
+        # for line in file:
+            # yield line.strip()
+
+# gen=file_read("file.txt")
+
+# print(next(file_read("file.txt")))
+# print("__________")
+
+# print(next(file_read("file.txt")))
