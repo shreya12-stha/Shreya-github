f44fd9c93e9a72cdd2486e42d39c8f19144e7507
Shreya <shreyastha1278@gmail.com>
2025-01-20 08:11:27 +0545

    built_in_function

---
+++ built_in_function.py
@@ -0,0 +1,29 @@
+# def cube(x):
+#     return x**3
+# number=[1,2,3,4,5]
+# cubed_number=map(cube,number)
+# print(list(cubed_number))
+
+# def odd_number(x):
+#     if x%2!=0:
+#         return x
+# number=[1,2,3,4,5,6,7,8]
+# result=filter(odd_number,number)
+# # print(list(result))
+
+# from functools import reduce
+# def product(x,y):
+#     return x*y
+# numbers=[1,2,3,4,5]
+# total=reduce(product,numbers)
+# print(total)
+
+# list,dict,tuple comprehension
+# decorster,Generator
+# module
+# impoet export virtual env 
+
+# git and github
+# database query
+# python with database
+# sql database xampp
